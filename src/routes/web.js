7ba39c531891b2e3ed6de1aa253d600e088331dd
NguyenKeo <nguyentrungnguyenth14@gmail.com>
2022-12-08 15:36:55 +0700

    video 3 - mvc

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import homeController from '../controller/homeController';
 
 const router = express.Router();
 
@@ -6,10 +7,11 @@ const router = express.Router();
  *
  * @param {*} app express app
  */
+
 const initWebRouter = app => {
-	router.get('/', (req, res) => {
-		return res.send('Hello world');
-	});
+	//path, handler
+	router.get('/', homeController.handleHelloWord);
+	router.get('/user', homeController.handleUserPage);
 
 	return app.use('/', router);
 };
